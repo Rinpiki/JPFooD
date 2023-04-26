f7bfae3b421246bf5ca5f59daceb90e676c3b281
Marcos-Pablo <rinpikibr@Gmail.com>
2023-04-26 17:41:06 -0300

    feat: add section feedback por all screens

--- src/componets/Feedback.tsx
+++ src/componets/Feedback.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import review1 from '../../public/img/review1.png';
+import review2 from '../../public/img/review2.png';
 import FeedbackCards from './subcomponets/FeedbackCards';
 
 function Feedback() {
   return (
-    <div className="flex flex-col items-center">
-      <h4 className="font-semibold text-2xl mb-10">Comentários</h4>
-      <FeedbackCards />
-      <FeedbackCards />
+    <div className="flex flex-col items-center md:items-start md:w-full">
+      <h4 className="font-semibold text-2xl mb-10 md:text-5xl md:mb-12">
+        Comentários
+      </h4>
+      <div className=" md:flex justify-between w-full md:space-x-5">
+        <FeedbackCards
+          image={review1}
+          name="Daniele Almeida"
+          review="Ótimo serviço! Encantada com a qualidade dos pratos."
+        />
+        <FeedbackCards
+          image={review2}
+          name="Ricardo França"
+          review="Não é apenas a comida excelente, o serviço torna a experiência especial."
+        />
+      </div>
     </div>
   );
 }
--- src/componets/subcomponets/FeedbackCards.tsx
+++ src/componets/subcomponets/FeedbackCards.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import Image, { StaticImageData } from 'next/image';
-import review1 from '../../../public/img/review1.png';
 import stars from '../../../public/icon/stars.svg';
 
-function FeedbackCards() {
+interface CardProps {
+  name: string;
+  review: string;
+  image: StaticImageData;
+}
+
+function FeedbackCards({ name, review, image }: CardProps) {
   return (
-    <div className="flex flex-col items-center text-center bg-[#F3F3F3] px-[22px] py-6 mb-7 rounded-lg">
+    <div className="flex flex-col max-w-[600px] items-center text-center bg-[#F3F3F3] px-[22px] py-6 mb-7 rounded-lg md:py-10 md:px-[40px] md:items-start md:text-start lg:rounded-2xl">
       <Image
-        src={review1}
-        width={60}
-        height={60}
+        src={image}
+        width={120}
+        height={120}
         alt="perfil foto"
-        className="mb-4"
+        className="mb-4 md:w-[120px] md:h-[120px] md:mb-6"
       />
-      <p className="mb-4 font-medium text-lg">Daniele Almeida</p>
-      <span className="mb-2 font-normal text-base text-[#616161]">
-        Ótimo serviço! Encantada com a qualidade dos pratos.
+      <p className="mb-4 font-medium text-lg md:text-[32px]">{name}</p>
+      <span className="mb-2 font-normal text-base font-Inter text-[#616161] md:text-2xl md:mb-4">
+        {review}
       </span>
       <Image src={stars} alt="perfil foto" />
     </div>
